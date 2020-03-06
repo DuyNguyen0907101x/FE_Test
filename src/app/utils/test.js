3fefb27c031e7e4c3b68139bd0a7ce813f0a8776
unknown <duy.nguyen2@be.xyz>
2020-03-06 08:26:49 +0700

    Duy Cao

---
+++ src/app/utils/test.js
@@ -0,0 +1,13 @@
+export default class Human {
+    constructor(name, age) {
+        this.name = name;
+    }
+
+    getName() {
+        return this.name;
+    }
+
+    setName(name) {
+        this.name = name;
+    }
+}
